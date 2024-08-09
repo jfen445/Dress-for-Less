34375ddfc4f10cd247db3638ef8cdf7987b8f422
jfen445 <jfen445@aucklanduni.ac.nz>
2024-08-09 19:06:28 +1000

    Remove redundant package

--- auth.ts
+++ auth.ts
@@ -1,13 +1,3 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "./lib/db/db";
-
-// export const {
-//   handlers: { GET, POST },
-//   auth,
-//   signIn,
-//   signOut,
-// } = NextAuth({
-//   providers: [Google],
-// });
