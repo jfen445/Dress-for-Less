a8658d224e01105b57f85dd1d1217e4cbcab07ba
jfen445 <jfen445@aucklanduni.ac.nz>
2024-12-16 20:45:32 +1100

    Remove lib.ts

--- lib.ts
+++ lib.ts
@@ -1,61 +1,61 @@
-import { SignJWT, jwtVerify } from "jose";
-import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
-import { UserType } from "./common/types";
+// import { SignJWT, jwtVerify } from "jose";
+// import { cookies } from "next/headers";
+// import { NextRequest, NextResponse } from "next/server";
+// import { UserType } from "./common/types";
 
-const secretKey = "secret";
-const key = new TextEncoder().encode(secretKey);
+// const secretKey = "secret";
+// const key = new TextEncoder().encode(secretKey);
 
-export async function encrypt(payload: any) {
-  return await new SignJWT(payload)
-    .setProtectedHeader({ alg: "HS256" })
-    .setIssuedAt()
-    .setExpirationTime("1 hour from now")
-    .sign(key);
-}
+// export async function encrypt(payload: any) {
+//   return await new SignJWT(payload)
+//     .setProtectedHeader({ alg: "HS256" })
+//     .setIssuedAt()
+//     .setExpirationTime("1 hour from now")
+//     .sign(key);
+// }
 
-export async function decrypt(input: string): Promise<any> {
-  const { payload } = await jwtVerify(input, key, {
-    algorithms: ["HS256"],
-  });
-  return payload;
-}
+// export async function decrypt(input: string): Promise<any> {
+//   const { payload } = await jwtVerify(input, key, {
+//     algorithms: ["HS256"],
+//   });
+//   return payload;
+// }
 
-export async function setLoginCookie(user: UserType) {
-  // Verify credentials && get the user
+// export async function setLoginCookie(user: UserType) {
+//   // Verify credentials && get the user
 
-  // Create the session
-  const expires = new Date(Date.now() + 10 * 1000);
-  const session = await encrypt({ user, expires });
+//   // Create the session
+//   const expires = new Date(Date.now() + 10 * 1000);
+//   const session = await encrypt({ user, expires });
 
-  // Save the session in a cookie
-  cookies().set("session", session, { expires, httpOnly: true });
-}
+//   // Save the session in a cookie
+//   cookies().set("session", session, { expires, httpOnly: true });
+// }
 
-export async function logout() {
-  // Destroy the session
-  cookies().set("session", "", { expires: new Date(0) });
-}
+// export async function logout() {
+//   // Destroy the session
+//   cookies().set("session", "", { expires: new Date(0) });
+// }
 
-export async function getSession() {
-  const session = cookies().get("session")?.value;
-  if (!session) return null;
-  return await decrypt(session);
-}
+// export async function getSession() {
+//   const session = cookies().get("session")?.value;
+//   if (!session) return null;
+//   return await decrypt(session);
+// }
 
-export async function updateSession(request: NextRequest) {
-  const session = request.cookies.get("session")?.value;
-  if (!session) return;
+// export async function updateSession(request: NextRequest) {
+//   const session = request.cookies.get("session")?.value;
+//   if (!session) return;
 
-  // Refresh the session so it doesn't expire
-  const parsed = await decrypt(session);
-  parsed.expires = new Date(Date.now() + 10 * 1000);
-  const res = NextResponse.next();
-  res.cookies.set({
-    name: "session",
-    value: await encrypt(parsed),
-    httpOnly: true,
-    expires: parsed.expires,
-  });
-  return res;
-}
+//   // Refresh the session so it doesn't expire
+//   const parsed = await decrypt(session);
+//   parsed.expires = new Date(Date.now() + 10 * 1000);
+//   const res = NextResponse.next();
+//   res.cookies.set({
+//     name: "session",
+//     value: await encrypt(parsed),
+//     httpOnly: true,
+//     expires: parsed.expires,
+//   });
+//   return res;
+// }
