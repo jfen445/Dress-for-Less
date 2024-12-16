c8c269c097d7ea2d09df30e47117ef2250ac17e5
jfen445 <jfen445@aucklanduni.ac.nz>
2024-12-16 20:50:00 +1100

    Remove login API

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,10 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
-import { serialize } from "cookie";
-import { encrypt } from "../../../lib";
-import { findUser } from "../../../lib/db/user-dao";
 import { dbConnect } from "../../../lib/db/db";
-import bcrypt from "bcrypt";
 
 export default async function handler(
   req: NextApiRequest,
@@ -12,34 +7,34 @@ export default async function handler(
 ) {
   await dbConnect();
 
-  const { email, password } = req.body;
-  // await signIn('credentials', { email, password })
+  // const { email, password } = req.body;
+  // // await signIn('credentials', { email, password })
 
-  const sessionData = req.body;
-  const encryptedSessionData = encrypt(sessionData);
+  // const sessionData = req.body;
+  // const encryptedSessionData = encrypt(sessionData);
 
-  const users = await findUser(email);
+  // const users = await findUser(email);
 
-  if (users.length === 0) {
-    res.status(404).json({
-      message: "Invalid account credentials",
-    });
-  }
+  // if (users.length === 0) {
+  //   res.status(404).json({
+  //     message: "Invalid account credentials",
+  //   });
+  // }
 
-  const match = await bcrypt.compareSync(password, users[0].password);
+  // const match = await bcrypt.compareSync(password, users[0].password);
 
-  if (match) {
-    const cookie = serialize("currentUser", await encryptedSessionData, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      maxAge: 60 * 60, // One hour
-      path: "/",
-    });
-    res.setHeader("Set-Cookie", cookie);
-    res.status(200).json({ message: "Successfully set cookie!" });
-  } else {
-    res.status(404).json({
-      message: "Invalid account credentials",
-    });
-  }
+  // if (match) {
+  //   const cookie = serialize("currentUser", await encryptedSessionData, {
+  //     httpOnly: true,
+  //     secure: process.env.NODE_ENV === "production",
+  //     maxAge: 60 * 60, // One hour
+  //     path: "/",
+  //   });
+  //   res.setHeader("Set-Cookie", cookie);
+  //   res.status(200).json({ message: "Successfully set cookie!" });
+  // } else {
+  //   res.status(404).json({
+  //     message: "Invalid account credentials",
+  //   });
+  // }
 }
