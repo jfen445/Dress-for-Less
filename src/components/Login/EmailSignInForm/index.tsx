e8d20e37174bd279fd1a1f086c5ca76352f9dc60
Josh <jfen445@aucklanduni.ac.nz>
2025-05-25 16:04:52 +1000

    Add log in loading

--- src/components/Login/EmailSignInForm/index.tsx
+++ src/components/Login/EmailSignInForm/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import { useSearchParams } from "next/navigation";
 import { signIn } from "next-auth/react";
 
@@ -7,14 +8,18 @@ import Input from "@/components/Input";
 import Button from "@/components/Button";
 
 export default function EmailSignInForm() {
+  const [isLoading, setIsLoading] = React.useState(false);
   const searchParams = useSearchParams();
   const callbackUrl = searchParams?.get("callbackUrl") || "/account";
 
   async function handleSubmit(event: any) {
+    setIsLoading(true);
     event.preventDefault();
     const formData = new FormData(event.target);
     const email = formData.get("email");
-    signIn("email", { email });
+    signIn("email", { email }).finally(() => {
+      setIsLoading(false);
+    });
   }
 
   return (
@@ -29,8 +34,13 @@ export default function EmailSignInForm() {
           required
         />
       </div>
-      <Button type="submit" color="gray" className="mt-4 w-full">
-        Continue with email
+      <Button
+        type="submit"
+        color="gray"
+        className="mt-4 w-full"
+        disabled={isLoading}
+      >
+        {isLoading ? "Loading..." : "Continue with email"}
       </Button>
     </form>
   );
