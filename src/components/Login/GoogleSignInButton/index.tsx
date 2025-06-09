42489de76fa50f4945b071f82297f4c085c5c046
Josh <jfen445@aucklanduni.ac.nz>
2025-06-09 15:09:59 +1000

    Handle login page

--- src/components/Login/GoogleSignInButton/index.tsx
+++ src/components/Login/GoogleSignInButton/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import { useSearchParams } from "next/navigation";
 import { signIn } from "next-auth/react";
 import Button from "@/components/Button";
@@ -7,11 +8,23 @@ import Button from "@/components/Button";
 const GoogleSignInButton = () => {
   const searchParams = useSearchParams();
   const callbackUrl = searchParams?.get("callbackUrl");
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const handleSignIn = async () => {
+    setIsLoading(true);
+    try {
+      await signIn("google");
+    } catch (error) {
+      console.error("Error signing in with Google:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <button
       className="w-full bg-black text-white flex items-center align-center p-2 rounded-md justify-center"
-      onClick={() => signIn("google")}
+      onClick={() => handleSignIn()}
     >
       <svg
         aria-hidden="true"
@@ -23,11 +36,11 @@ const GoogleSignInButton = () => {
         viewBox="0 0 488 512"
       >
         <path
-          fill="red"
+          fill="#fff" // Light mode: white fill
           d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
         ></path>
       </svg>
-      Continue with Google
+      {isLoading ? "Logging in..." : "Continue with Google"}
     </button>
   );
 };
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,9 +1,7 @@
-import { useRouter } from "next/navigation";
 import * as React from "react";
-import Toast from "../Toast";
 import Link from "next/link";
 
-import { useSession, signIn } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import Button from "../Button";
 import EmailSignInForm from "./EmailSignInForm";
 import GoogleSignInButton from "./GoogleSignInButton";
@@ -17,21 +15,36 @@ const LoginComponent = () => {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             {session && session.user
-              ? `Welcome ${session && session.user ? session.user.name : ""}`
+              ? `Welcome ${
+                  session && session.user && session.user.name
+                    ? session.user.name
+                    : ""
+                }`
               : "Sign in to your account"}
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           {session ? (
-            <Link href="/dresses" className="group -m-2 flex items-center p-2">
-              <Button className="mx-auto w-full">Start browsing now!</Button>
-            </Link>
+            <>
+              <Link
+                href="/account"
+                className="group -m-2 flex items-center p-2"
+              >
+                <Button className="mx-auto w-full">
+                  Complete your profile to making bookings
+                </Button>
+              </Link>
+              <Link
+                href="/dresses"
+                className="group -m-2 flex items-center p-2"
+              >
+                <Button variant={"secondary"} className="mx-auto w-full">
+                  Start browsing now!
+                </Button>
+              </Link>
+            </>
           ) : (
-            // <Button className="mx-auto w-full" onClick={() => signIn()}>
-            //   Click here to sign on wtih Google
-            // </Button>
-
             <div className="sm:rounded-5xl mt-2 flex-auto bg-white shadow-2xl shadow-gray-900/10 py-8 px-4">
               <EmailSignInForm />
               <div className="mx-auto my-10 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
