9c9c8f5076327ca237ba57a7e4e99d0a8b6d0147
Josh <jfen445@aucklanduni.ac.nz>
2026-05-31 00:33:25 +1000

    Add login route redirect

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -2,8 +2,19 @@
 
 import LoginComponent from "@/components/Login";
 import * as React from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
 const LoginPage = () => {
+  const { status } = useSession();
+  const router = useRouter();
+
+  React.useEffect(() => {
+    if (status === "authenticated") router.replace("/");
+  }, [status]);
+
+  if (status === "loading" || status === "authenticated") return null;
+
   return (
     <>
       <LoginComponent />
