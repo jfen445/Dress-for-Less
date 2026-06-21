65115d4e7ce5bcffe128739e8fa6fccc7fd57a71
Denis <denis.lynn.code@gmail.com>
2026-06-21 14:56:26 +1000

    opacity and border less visible on non-editable fields

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -40,7 +40,7 @@ const Input: React.FC<InputProps> = ({
       disabled={disabled ?? false}
       placeholder={placeholder}
       autoComplete={autoComplete}
-      className={`${className} border border-rose-900 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5`}
+      className={`${className} border border-rose-900 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 disabled:border-black disabled:opacity-50`}
       {...props}
     />
   );
