e7062d3529b505c7bb7e8265f937dedb4247a252
Denis <denis.lynn.code@gmail.com>
2026-07-02 21:28:54 +1000

    fix css nesting bro

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    "tailwindcss/nesting": {}, // 1. Nesting plugin runs first to unpack nested rules
+    tailwindcss: {}, // 2. Tailwind runs second to process utility classes
+    autoprefixer: {}, // 3. Autoprefixer runs last to add vendor prefixes
   },
 };
