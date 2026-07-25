b03c9aa858b703709ac2716238b7487e9db13c47
Josh <jfen445@aucklanduni.ac.nz>
2026-07-25 20:35:06 +1000

    Improve sanity caching

--- pages/api/sanity/faq.ts
+++ pages/api/sanity/faq.ts
@@ -1,20 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getFaq } from "../../../sanity/sanity.query";
 
-let cachedData: any = null;
-let lastFetched = 0;
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const CACHE_DURATION = 3600000; // Cache for 1 hour
-  const now = Date.now();
+  const data = await getFaq();
 
-  if (!cachedData || now - lastFetched > CACHE_DURATION) {
-    cachedData = await getFaq();
-    lastFetched = now;
-  }
-
-  res.status(200).json(cachedData);
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=3600, stale-while-revalidate=86400"
+  );
+  res.status(200).json(data);
 }
